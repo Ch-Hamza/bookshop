fdf8fd263305e779d4088f02362b4e53aae0657b
GhassenChaar <chaarghassen@gmail.com>
2018-12-06 17:57:05 +0100

    Add All Files

---
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
+import { AppUser } from '../models/app-user';
+@Component({
+  selector: 'app-bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+  styleUrls: ['./bs-navbar.component.css']
+})
+export class BsNavbarComponent {
+  appUser: AppUser;
+
+  constructor(private auth: AuthService) {
+    auth.appUser$.subscribe(appUser => this.appUser = appUser);
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+
+}
